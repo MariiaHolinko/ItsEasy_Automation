6e2895acc38970d084d98f3aed7a5b8aaba36ce9
[MariiaHolinko] <[holinkomariia@gmail.com]>
2025-05-06 19:52:40 +0200

    leave one test

--- helpers/loginSetup.js
+++ helpers/loginSetup.js
@@ -5,7 +5,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 (async () => {
-  const browser = await chromium.launch({ headless: false });
+  const browser = await chromium.launch({ headless: true });
   const context = await browser.newContext();
   const page = await context.newPage();
 
