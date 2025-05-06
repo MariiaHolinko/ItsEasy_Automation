2ceafd13d75c7cb4e68d402b3d20d4ca2a317a64
[MariiaHolinko] <[holinkomariia@gmail.com]>
2025-05-06 19:42:24 +0200

    leave one test

--- tests/prodTests/filterByCategory.spec.js
+++ tests/prodTests/filterByCategory.spec.js
@@ -1,36 +1,36 @@
-import { expect } from '@playwright/test';
-import { test } from '../../utils/fixtures.js';
-import { HomePage } from '../../pages/Prod/homePage.js';
+// import { expect } from '@playwright/test';
+// import { test } from '../../utils/fixtures.js';
+// import { HomePage } from '../../pages/Prod/homePage.js';
 
-test.beforeEach(async ({ page }) => {
-    await page.goto(process.env.URL); 
-});
+// test.beforeEach(async ({ page }) => {
+//     await page.goto(process.env.URL); 
+// });
 
-test('User can filter courses by each category', async ({ page, AllCourses }) => {
+// test('User can filter courses by each category', async ({ page, AllCourses }) => {
 
-    await HomePage.clickAllCoursesLink();
-    const categoryInput = await AllCourses.clickCategoryField();
+//     await HomePage.clickAllCoursesLink();
+//     const categoryInput = await AllCourses.clickCategoryField();
     
 
-  await categoryInput.click();
+//   await categoryInput.click();
 
-  const options = page.locator('[role="listbox"] [role="option"]');
-  const count = await options.count();
-  expect(count).toBeGreaterThan(0);
+//   const options = page.locator('[role="listbox"] [role="option"]');
+//   const count = await options.count();
+//   expect(count).toBeGreaterThan(0);
 
-  for (let i = 0; i < count; i++) {
-    await categoryInput.click(); // открыть дропдаун заново
-    const option = options.nth(i);
-    const categoryName = await option.innerText();
-    await option.click();
+//   for (let i = 0; i < count; i++) {
+//     await categoryInput.click(); // открыть дропдаун заново
+//     const option = options.nth(i);
+//     const categoryName = await option.innerText();
+//     await option.click();
 
-    const courseItems = page.locator('[data-testid="course-item"]');
-    await expect(courseItems).toHaveCountGreaterThan(0);
+//     const courseItems = page.locator('[data-testid="course-item"]');
+//     await expect(courseItems).toHaveCountGreaterThan(0);
 
-    const titles = await courseItems.allInnerTexts();
-    for (const title of titles) {
-      expect(title.toLowerCase()).toContain(categoryName.toLowerCase());
-    }
-  }
-});
+//     const titles = await courseItems.allInnerTexts();
+//     for (const title of titles) {
+//       expect(title.toLowerCase()).toContain(categoryName.toLowerCase());
+//     }
+//   }
+// });
 
